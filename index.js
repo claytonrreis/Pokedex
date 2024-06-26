a072de842dbb77d4c99172d9af2f74a09080c78b
claytonrreis <103720189+claytonrreis@users.noreply.github.com>
2024-06-26 16:55:13 -0500

    add all pokemons

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const pokeContainer = document.getElementById("poke-container");
-const pokemonCount = 150;
+const pokemonCount = 1025;
+
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const main_types = Object.keys(colors);
+console.log(main_types);
 
 //arrow syntax
 const fetchPokemons = async () => {
@@ -24,20 +44,44 @@ const createPokenonCard = (pokemon) => {
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
 
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+  console.log(color);
+
+  pokemonEl.style.backgroundColor = color;
+
   const pokemonInnerHTML = `
-    <div class="img-container">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
-          alt=""
-        />
+  <div class="img-container">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+        alt=""
+      />
+    </div>
+      <div class="info">
+        <span class="number">${pokemon.id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type"> Type <span>${type}</span></small>
       </div>
-        <div class="info">
-          <span class="number">${pokemon.id}</span>
-          <h3 class="name">${pokemon.name}</h3>
-          <small class="type"> Type <span>${pokemon.types[0].type.name}</span></small>
-        </div>
-      </div>
-    `;
+    </div>
+  `;
+
+  //   const pokemonInnerHTML = `
+  //     <div class="img-container">
+  //         <img
+  //           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+  //           alt=""
+  //         />
+  //       </div>
+  //         <div class="info">
+  //           <span class="number">${pokemon.id}</span>
+  //           <h3 class="name">${pokemon.name}</h3>
+  //           <small class="type"> Type <span>${pokemon.types[0].type.name}</span></small>
+  //         </div>
+  //       </div>
+  //     `;
   pokemonEl.innerHTML = pokemonInnerHTML;
   pokeContainer.appendChild(pokemonEl);
 };
